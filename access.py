5fd685c549f1bce254bc95a289f4c67d42dbcabd
bronxi47 <hbenja47@gmail.com>
2022-01-07 17:20:07 -0300

    Quitada la mano de la respuesta de Estoy trabajando para sellar el tweet...

--- access.py
+++ access.py
@@ -1,12 +1,12 @@
 # Credentials for your Twitter bot account
 
 # Consumer:
-CONSUMER_KEY    = 'CONSUMEY_KEY'
-CONSUMER_SECRET = 'CONSUMER_SECRETxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+CONSUMER_KEY    = 'v9svL3vQLzceDQ9KKQ5rAbWKP'
+CONSUMER_SECRET = 'ey5HcLzLj8iOdQ5wVtPjgfIABviBX2rBAm8qkD5blCFFBkZJ6j'
 
 # Access:
-ACCESS_TOKEN  = 'ACCESS_TOKEN-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-ACCESS_SECRET = 'ACCESS_SECRET-asdsadHPYnQSI06aQRk6U1zdT7WPhzA8nSTV1'
+ACCESS_TOKEN  = '1441029412166770689-RDzklTAt2jyC9APJTQAVVVjbTV7uJs'
+ACCESS_SECRET = 'B0BMlEwRILNY43kMv1hUurwMfoJoFMPqXjqxE8iTENAqb'
 
 # Constata pass:
-CONSTATA_PASS = 'CONSTATACLI-PASS'
+CONSTATA_PASS = 'twitterlovesconstata2021'
